e0683cdf273f6987a9724c4a8fbd713e6b563fee
david2joh <david2joh@gmail.com>
2022-03-10 12:28:30 -0500

    Part 2 of demostration Add log4j.properties

--- HibernateJPABeginner/src/main/java/com/test/project/App.java
+++ HibernateJPABeginner/src/main/java/com/test/project/App.java
@@ -1,8 +1,29 @@
 package com.test.project;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.test.hib.controller.findUser_Hql;
+import com.test.hib.model.User;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        findUser_Hql u = new findUser_Hql();
+        // Find/Sect * all users
+        u.findUser();
+        //Find/Select * all users with a iteration over the returned list of users
+        u.findUserSelect();
+        //Find/Select * from users where userID < 5
+        u.getRecordbyId();
+        //Determine the max salary of users
+        u.getmaxSalary();
+        //Find how many users we have
+        u.getnumUsers();
+        //Basic Named Query example , for now getting userID == 2
+        u.NamedQueryExample();
+
     }
 
 }
